1b911bec55ed4211795374cc1584c6864481c4e5
Suman Mudigonda <suman.mudigonda@creditculture.sg>
2019-07-17 16:04:35 +0800

    Initialized Microservice project using spring boot

---
+++ src/main/java/com/singtel/assignment/animal/SingtelMicroservicesAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.singtel.assignment.animal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingtelMicroservicesAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingtelMicroservicesAssignmentApplication.class, args);
+	}
+
+}
